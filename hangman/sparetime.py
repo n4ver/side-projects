4e0c0816244da9edfcd4b2775efbc315c6935f44
n4ver <58014561+n4ver@users.noreply.github.com>
2022-09-20 16:29:51 +0800

    Add files via upload

---
+++ hangman/sparetime.py
@@ -0,0 +1,81 @@
+import random
+
+
+class Hangman:
+    def __init__(self, path):
+        self.words = self.wordlist(path)
+        self.wrong_guesses = 5
+
+        
+    def screen(self, word, placeholder, guesses):
+        print(placeholder)
+        print()
+        print(f"Guessed Letters: {guesses}") # Clean up
+
+
+    def wordlist(self, path):
+        with open(path) as f:
+            lst = [i.strip("\n") for i in f.readlines()]
+            f.close()
+        return lst
+
+
+    def start(self):
+        word = random.choice(self.words).lower()
+        placeholder = self.create_placeholder(word)
+        guesses = []
+        self.gameloop(word, placeholder, guesses)
+
+
+    def create_placeholder(self, word):
+        length = len(word)
+        placeholder = "_ " * length
+        return placeholder.strip(" ")
+
+
+    def update_placeholder(self, word, placeholder, guess):
+        new_holder = ""
+        placeholder = placeholder.replace(" ", "")
+        for i in range(len(placeholder)):
+            if placeholder[i] == " ": continue
+            if placeholder[i] == "_":
+                if word[i] == guess:
+                    new_holder += guess + " "
+                else:
+                    new_holder += "_ "
+            else:
+                new_holder += placeholder[i] + " "
+        return new_holder.strip(" ")
+
+
+    def round(self, word, placeholder, guesses):
+        self.screen(word, placeholder, guesses)
+        guess = input("Give a guess: ")
+        while not guess.isalpha() or len(guess) != 1:
+            guess = input("Give a guess (a letter this time): ")
+        if guess in word:
+            placeholder = self.update_placeholder(word, placeholder, guess)
+        else:
+            guesses += [guess]
+        return [placeholder, guesses]
+
+
+    def gameloop(self, word, placeholder, guesses):
+        while placeholder.replace(" ", "") != word:
+            placeholder, guesses = self.round(word, placeholder, guesses)
+            if len(guesses) > self.wrong_guesses:
+                break
+        if placeholder.replace(" ", "") == word:
+            print("CONGRATULATIONS!!!")
+        else:
+            print("L")
+        print(f"The word was: {word}")
+        return
+
+
+def main():
+    game = Hangman(r"C:\Users\Nick\Desktop\wordlist.txt")
+    game.start()
+
+if __name__ == "__main__":
+    main()
